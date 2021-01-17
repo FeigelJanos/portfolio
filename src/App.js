af214778f9b41b8c77686f9b1bea023e974ccc90
Feigel <feigel_janos@yahoo.com>
2021-01-18 00:15:18 +0100

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Title</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Nav, NavbarContainer } from "./modules/Navbar.elements";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>Test</NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
